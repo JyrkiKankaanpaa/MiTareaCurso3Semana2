d093a1ed494ee08e9b36d6844cefe1f016b5494f
Jyrki Kankaanpää <jyrki.kankaanpaa@netikka.fi>
2016-06-20 14:36:43 +0300

    Some last fixes

--- app/src/main/java/com/curso/jyrki/mitareacurso3semana2/ActivityDos.java
+++ app/src/main/java/com/curso/jyrki/mitareacurso3semana2/ActivityDos.java
@@ -5,34 +5,37 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class ActivityDos extends AppCompatActivity {
+
+    TextView dosNobre;
+    TextView dosTele;
+    TextView dosEmail;
+    TextView dosDes;
+    TextView dosFecha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_dos);
 
+        Bundle dosBundle = getIntent().getExtras();
 
-        Intent intent = getIntent();
+        dosNobre = (TextView) findViewById(R.id.nombreCompleto_dos);
+        dosNobre.setText((CharSequence) dosBundle.get("send_dNobre"));
 
-        String nombreCompleto_campo_dos = intent.getStringExtra("nombreCompleto_campo_value");
-        String fechaDatePicker_dos = intent.getStringExtra("fechaDatePicker_value");
-        String teléfono_campo_dos = intent.getStringExtra("teléfono_campo_value");
-        String email_campo_dos = intent.getStringExtra("email_campo_value");
-        String descripción_campo_dos = intent.getStringExtra("descripción_campo_value");
+        dosTele = (TextView) findViewById(R.id.teléfono_dos);
+        dosTele.setText((CharSequence) dosBundle.get("send_dTele"));
 
+        dosEmail = (TextView) findViewById(R.id.email_dos);
+        dosEmail.setText((CharSequence) dosBundle.get("send_dEmail"));
 
-        Button miBoton2 =(Button)findViewById(R.id.activitybutton1);
-        miBoton2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent miIntent = new Intent(getApplicationContext(),ActivityDos.class);
-                startActivity(miIntent);
+        dosDes = (TextView) findViewById(R.id.descripción_dos);
+        dosDes.setText((CharSequence) dosBundle.get("send_dDes"));
 
-            }
-        });
-
+        dosFecha = (TextView) findViewById(R.id.fecha_dos);
+        dosFecha.setText((CharSequence) dosBundle.get("send_dFecha"));
 
     }
 }
--- app/src/main/java/com/curso/jyrki/mitareacurso3semana2/MainActivity.java
+++ app/src/main/java/com/curso/jyrki/mitareacurso3semana2/MainActivity.java
@@ -3,26 +3,55 @@ package com.curso.jyrki.mitareacurso3semana2;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    Button miBoton;
+    EditText mNobre;
+    EditText mFecha;
+    EditText mTele;
+    EditText mEmail;
+    EditText mDes;
+    DatePicker dp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button miBoton =(Button)findViewById(R.id.activitybutton1);
-        miBoton.setOnClickListener(new View.OnClickListener() {
+        miBoton =(Button)findViewById(R.id.activitybutton1);
+        mNobre = (EditText) findViewById(R.id.nombreCompleto_campo);
+      //  mFecha = (EditText) findViewById(R.id.nombreCompleto_campo);
+        mTele = (EditText) findViewById(R.id.teléfono_campo);
+        mEmail = (EditText) findViewById(R.id.email_campo);
+        mDes = (EditText) findViewById(R.id.descripción_campo);
+        dp = (DatePicker) findViewById(R.id.datePicker);
+
+        miBoton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent miIntent = new Intent(getApplicationContext(),ActivityDos.class);
-                miIntent.putExtra("nombreCompleto_campo_value",findViewById(R.id.nombreCompleto_campo).getContext());
-                miIntent.putExtra("fechaDatePicker_value",findViewById(R.id.fechaDatePicker).getContext());
-                miIntent.putExtra("teléfono_campo_value",findViewById(R.id.teléfono_campo).getContext());
-                miIntent.putExtra("email_campo_value",findViewById(R.id.email_campo).getContext());
-                miIntent.putExtra("descripción_campo_value",findViewById(R.id.descripción_campo).getContext());
+                String dNobre = mNobre.getText().toString();
+                String dTele = mTele.getText().toString();
+                String dEmail = mEmail.getText().toString();
+                String dDes = mDes.getText().toString();
+                String dFecha = dp.getDayOfMonth()+"/"+ dp.getMonth()+"/"+ dp.getYear();
+
+                Log.d("MainActivity", "main*********************" + mNobre);
+
+                Intent miIntent = new Intent("second_filter");
+                Bundle miBundle = new Bundle();
+                miBundle.putString("send_dNobre",dNobre);
+                miBundle.putString("send_dTele",dTele);
+                miBundle.putString("send_dEmail",dEmail);
+                miBundle.putString("send_dDes",dDes);
+                miBundle.putString("send_dFecha",dFecha);
+                miIntent.putExtras(miBundle);
+
                 startActivity(miIntent);
 
             }
